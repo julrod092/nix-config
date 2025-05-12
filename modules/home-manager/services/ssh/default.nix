aa6e579eeb8f840b71898f9bf0b5d7bd59df4db2
Julian Rodriguez <jrodriguezrpo@proton.me>
2025-05-12 11:21:19 -0500

    Modify ssh

--- modules/home-manager/services/ssh/default.nix
+++ modules/home-manager/services/ssh/default.nix
@@ -2,14 +2,22 @@
   # Manage kanshi services via Home-manager
   programs.ssh = {
     enable = true;
-    matchBlocks = lib.mkIf (!pkgs.stdenv.isDarwin) {
+    matchBlocks = lib.mkIf (pkgs.stdenv.isDarwin) {
       "jandresrodriguez-ncl.github.com" = {
         hostname = "github.com";
         user = "jandresrodriguez-ncl";
       };
+      "julrod092.github.com" = {
+        hostname = "github.com";
+        user = "julrod092";
+      };
       work = lib.hm.dag.entryBefore ["jandresrodriguez-ncl.github.com"] {
         hostname = "github.com";
         identityFile = "~/.ssh/ncl-ssh";
+      };
+      personal = lib.hm.dag.entryBefore ["julrod092.github.com"] {
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_ed25519";
       };
     };
   };
