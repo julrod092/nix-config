f2653f4decd2b0a84f3643e84e8135592defc881
Julian Rodriguez <jrodriguezrpo@pm.me>
2025-06-04 19:39:22 -0500

    Add configuration for synergy

---
+++ modules/nixos/services/synergy/default.nix
@@ -0,0 +1,56 @@
+{ config, pkgs, ... }: {
+
+  # Install synergy package
+  environment.systemPackages = with pkgs; [
+    synergy
+  ];
+
+  # Copy synergy configuration files
+  environment.etc."synergy/synergy.conf".source = ../../../../files/synergy.conf;
+  environment.etc."synergy/SSL/Synergy.pem".source = ../../../../files/synergy-ssl/Synergy.pem;
+
+  # Synergy server systemd user service template
+  systemd.user.services.synergy-server = {
+    description = "Synergy Server";
+    after = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    wantedBy = [ "default.target" ];
+    
+    environment = {
+      DISPLAY = ":0";
+      WAYLAND_DISPLAY = "wayland-0";
+      XDG_RUNTIME_DIR = "/run/user/1000";
+    };
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.synergy}/bin/synergys -f --config /etc/synergy/synergy.conf --enable-crypto --tls-cert /etc/synergy/SSL/Synergy.pem";
+      Restart = "always";
+      RestartSec = "10";
+    };
+  };
+
+  # Enable lingering for the main user to start user services on boot
+  systemd.services.enable-synergy-linger = {
+    description = "Enable lingering for synergy user services";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+      ExecStart = "${pkgs.systemd}/bin/loginctl enable-linger julrod";
+      ExecStop = "${pkgs.systemd}/bin/loginctl disable-linger julrod";
+    };
+  };
+
+  # Open firewall for synergy
+  networking.firewall = {
+    allowedTCPPorts = [ 24800 ];
+    allowedUDPPorts = [ 24800 ];
+  };
+
+  # Set proper permissions for SSL certificate
+  system.activationScripts.synergy-ssl = ''
+    chmod 600 /etc/synergy/SSL/Synergy.pem
+    chown root:users /etc/synergy/SSL/Synergy.pem
+  '';
+} 
