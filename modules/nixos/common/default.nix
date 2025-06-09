ac0f8c0ec2475ed5e4ffb75410a228e07f9422ee
Julian Rodriguez <jrodriguezrpo@proton.me>
2025-06-09 09:03:42 -0500

    Update neovim and boot initialization

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -47,6 +47,13 @@
         configurationLimit = 5;
       };
       efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        version = 2;
+        device = "nodev";
+        efiSupport = true;
+        useOSProber = true;
+      };
     };
     plymouth.enable = true;
   };
