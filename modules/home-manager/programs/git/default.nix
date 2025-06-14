fed22b3c7986386e503e93310fb93ad96ad77ba1
Julian Rodriguez <jrodriguezrpo@proton.me>
2025-06-14 16:04:56 -0500

    modify git key to use gpg signing

--- modules/home-manager/programs/git/default.nix
+++ modules/home-manager/programs/git/default.nix
@@ -4,10 +4,10 @@
     enable = true;
     userName = userConfig.fullName;
     userEmail = userConfig.email;
-    # signing = {
-    #   key = userConfig.gitKey;
-    #   signByDefault = true;
-    # };
+    signing = {
+      key = userConfig.gitKey;
+      signByDefault = true;
+    };
     delta = {
       enable = true;
       options = {
