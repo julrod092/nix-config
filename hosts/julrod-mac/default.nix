15e90cb39bdcb76006520f363a328ef81667f802
Julian Rodriguez <jrodriguezrpo@pm.me>
2025-06-23 15:49:31 -0500

    Add intellij using dock lib in nix

--- hosts/julrod-mac/default.nix
+++ hosts/julrod-mac/default.nix
@@ -132,7 +132,7 @@
       { path = "/Applications/Zen.app/"; }
       { path = "/Applications/Microsoft\ Teams.app/"; }
       { path = "/Applications/AppCleaner.app/"; }
-      # { path = "/Applications/IntelliJ\ IDEA.app/"; }
+      { path = "/Applications/IntelliJ\ IDEA.app/"; }
       { path = "${pkgs.alacritty}/Applications/Alacritty.app/"; }
       # { path = "${pkgs.stable.synergy}/Applications/Synergy.app/"; }
       # { path = "${pkgs.stable.jetbrains.idea-ultimate}/Applications/IntelliJ\ IDEA.app/"; }
