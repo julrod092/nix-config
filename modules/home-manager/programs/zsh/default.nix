b63ec84189a19810faea8d0ab2362da7d4c6f8b0
Julian Rodriguez <jrodriguezrpo@pm.me>
2025-07-04 15:09:22 -0500

    Update sops for interopeatibility

--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, hostname, userConfig, config, ... }:
+{ pkgs, hostname, userConfig, config, lib, ... }:
 let
   system-rebuild = if pkgs.stdenv.isDarwin
   then "darwin-rebuild"
@@ -31,12 +31,9 @@ in
     };
 
     localVariables = {
-      NPM_GITHUB_TOKEN =
-        if pkgs.stdenv.isDarwin
-        then "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"
-        else "";
-      JAVA_HOME="${pkgs.zulu11}/bin";
-      M2_HOME="${pkgs.maven}/bin";
+      NPM_GITHUB_TOKEN = lib.mkIf (pkgs.stdenv.isDarwin) ''$(cat ${config.secrets.npm_github_token.path})'';
+      JAVA_HOME = "${pkgs.zulu11}/bin";
+      M2_HOME = "${pkgs.maven}/bin";
     };
 
     initContent = ''
