025b747913ae6669d1dc68239cf0638160d54648
Julian Rodriguez <jrodriguezrpo@pm.me>
2025-07-19 13:12:19 -0500

    Enable synergy ssl crypto

--- modules/home-manager/services/synergy/default.nix
+++ modules/home-manager/services/synergy/default.nix
@@ -10,7 +10,7 @@ let
 
       Service = {
         Type = "forking";
-        ExecStart = "${pkgs.synergy}/bin/synergys -f --config %h/.config/synergy/synergy.conf";
+        ExecStart = "${pkgs.synergy}/bin/synergys -f --config %h/.config/synergy/synergy.conf --enable-crypto";
         Restart = "always";
         RestartSec = "10";
         Environment = [
@@ -36,7 +36,7 @@ let
 
       Service = {
         Type = "forking";
-        ExecStart = "${pkgs.synergy}/bin/synergyc --name ${hostname} 192.168.68.58";
+        ExecStart = "${pkgs.synergy}/bin/synergyc --enable-crypto --name ${hostname} 192.168.68.58";
         Restart = "always";
         RestartSec = "10";
         Environment = [
