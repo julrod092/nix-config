05a04577e79a36bdc5be07539c6f94e093bf5c9d
Julian Rodriguez <jrodriguezrpo@pm.me>
2025-09-11 10:53:25 -0500

    Remove sessions variables from zsh to home config

--- modules/home-manager/common/default.nix
+++ modules/home-manager/common/default.nix
@@ -4,8 +4,24 @@
   userConfig,
   pkgs,
   system,
+  config,
   ...
-}: {
+}: 
+let
+  sharedVariables = {
+    EDITOR = "nvim";
+  };
+
+  darwinVariables = {
+    JAVA_HOME = "${pkgs.zulu11}/bin";
+    REPO_ACCESS = "$(cat ${config.sops.secrets."github_repo_token_access".path} 2>/dev/null || echo '')";
+    NPM_GITHUB_TOKEN = "$(cat ${config.sops.secrets."npm_github_token".path} 2>/dev/null || echo '')";
+    M2_HOME = "${pkgs.maven}/bin";
+  };
+
+  linuxVariables = {};
+in
+{
   imports = [
     ../programs/aerospace
     ../programs/alacritty
@@ -50,9 +66,9 @@
       if pkgs.stdenv.isDarwin
       then "/Users/${userConfig.name}"
       else "/home/${userConfig.name}";
-    sessionVariables = {
-       EDITOR = "nvim";
-     };
+    sessionVariables = sharedVariables 
+      // (lib.optionalAttrs pkgs.stdenv.isDarwin darwinVariables)
+      // (lib.optionalAttrs (!pkgs.stdenv.isDarwin) linuxVariables);
   };
 
   # Catpuccin flavor and accent
--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -4,26 +4,13 @@ let
   then "darwin-rebuild"
   else "nixos-rebuild";
 
-  sharedVariables = {};
-
-  darwinVariables = {
-    JAVA_HOME="${pkgs.zulu11}/bin";
-    REPO_ACCESS = "$(cat ${config.sops.secrets."github_repo_token_access".path} 2>/dev/null || echo '')";
-    NPM_GITHUB_TOKEN = "$(cat ${config.sops.secrets."npm_github_token".path} 2>/dev/null || echo '')";
-    M2_HOME = "${pkgs.maven}/bin";
-  };
-
-  linuxVariables = {};
+  
 in
 {
 
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-
-    sessionVariables = sharedVariables 
-      // (lib.optionalAttrs pkgs.stdenv.isDarwin darwinVariables)
-      // (lib.optionalAttrs (!pkgs.stdenv.isDarwin) linuxVariables);
 
     shellAliases = {
       ff = "fastfetch";
