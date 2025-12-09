f2f7fc15f544d8856659ad686938efa79f994695
Julian Andres Rodriguez Restrepo <jrodriguezrpo@pm.me>
2025-12-08 20:00:01 -0500

    After update changes

--- modules/home-manager/services/ssh/default.nix
+++ modules/home-manager/services/ssh/default.nix
@@ -1,6 +1,7 @@
 { lib, pkgs, ... }: {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
     matchBlocks = lib.mkIf (pkgs.stdenv.isDarwin) {
       "work" = {
         hostname = "github.com";
