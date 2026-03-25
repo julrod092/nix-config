bdb03d44b40b28162e4f742da98f4208e5d80e44
Julian Rodriguez <jrodriguezrpo@pm.me>
2026-03-25 07:21:10 -0500

    Another disk

--- hosts/nix-desktop/default.nix
+++ hosts/nix-desktop/default.nix
@@ -23,6 +23,16 @@
   # Set hostname
   networking.hostName = hostname;
 
+  fileSystems."/home/julrod/m2" = {
+    device = "/dev/disk/by-uuid/f943f8f5-5917-41bf-9a39-382a04fb1986";
+    fsType = "ext4";
+    options = [ "defaults" "nofail" ];
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /home/julrod/m2 0775 julrod users - -"
+  ];
+
   # Enable Razer Nari Ultimate headset profiles
   # hardware.razer-nari.enable = true;
 
