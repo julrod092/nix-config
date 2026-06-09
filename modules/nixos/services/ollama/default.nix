54c8f4469ece1d09fc32e2d5577319057953035b
Julian Rodriguez <jrodriguezrpo@pm.me>
2026-06-09 09:49:13 -0500

    Update Ollama configuration

--- modules/nixos/services/ollama/default.nix
+++ modules/nixos/services/ollama/default.nix
@@ -4,6 +4,11 @@
     package = pkgs.ollama-cuda;
     host = "0.0.0.0";
     openFirewall = true;
+    acceleration = "cuda";
+    environmentVariables = {
+      OLLAMA_KEEP_ALIVE = "30m";
+      OLLAMA_LOAD_TIMEOUT = "10m";
+    };
     loadModels = ["qwen3-coder:30b"];
   };
 }
