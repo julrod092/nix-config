025c75de14aef9dcab1e2db84615717056bb649f
Julian Rodriguez <jandresrodriguez@nclcorp.com>
2026-06-15 10:14:35 -0500

    Update conhfig to delete warning

--- modules/home-manager/services/ssh/default.nix
+++ modules/home-manager/services/ssh/default.nix
@@ -6,7 +6,7 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin) {
+    settings = lib.mkIf (pkgs.stdenv.hostPlatform.isDarwin) {
       "work" = {
         hostname = "github.com";
         user = "git";
