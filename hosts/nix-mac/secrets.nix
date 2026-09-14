ed3706be28a9c926f035d3cc261681ae0106954f
Julian Rodriguez <jrodriguezrpo@pm.me>
2026-09-14 10:25:40 -0500

    Change secret creation on secrets nix file

--- hosts/nix-mac/secrets.nix
+++ hosts/nix-mac/secrets.nix
@@ -1,4 +1,8 @@
-{lib, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   sops = {
     defaultSopsFile = ./secrets.yaml;
     secrets = lib.genAttrs [
@@ -8,4 +12,8 @@
       "tf_gateway_api_key"
     ] (_: {});
   };
+
+  programs.zsh.initContent = lib.mkAfter ''
+    export TF_GATEWAY_API_KEY=$(cat ${config.sops.secrets.tf_gateway_api_key.path})
+  '';
 }
--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   hostname,
-  config,
   ...
 }: let
   systemRebuild =
@@ -34,8 +33,6 @@ in {
           prompt_segment black default "%(!.%{%F{yellow}%}.) λ "
         fi
       }
-
-      export TF_GATEWAY_API_KEY=$(cat ${config.sops.secrets.tf_gateway_api_key.path})
     '';
   };
 }
